b53ca044990345666ae223cc528447cac1c23463
mekkaoui7 <ahmedmike350@gmail.com>
2022-03-15 18:06:07 +0000

    only configuration

--- views/src/App.js
+++ views/src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './HomePage/HomePage';
 import Login from './AuthPage/Login';
-
+import Landing from './landing-page/Landing';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/auth/login" element={<Login />} />
+        <Route exact path='/landing' element={<Landing/>}/>  
       </Routes>
     </div>
   );
