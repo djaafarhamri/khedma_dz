74c72946749490fb75aeee036648ca14f723bd2b
djaafarhamri <hamridjaafar12@gmail.com>
2022-04-14 16:22:51 +0100

    frontend structure

--- views/src/App.js
+++ views/src/App.js
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage';
-import Login from './pages/AuthPage/Login';
-import Register from './pages/AuthPage/Register';
+import Home from './pages/HomePage/Home';
+import Login from './pages/LoginPage/Login';
+import Profile from './pages/ProfilePage/Profile';
+import Register from './pages/RegisterPage/Register';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
+        <Route exact path="/profile/:username" element={<Profile />} />
       </Routes>
     </div>
   );
--- views/src/pages/HomePage/Home.jsx
+++ views/src/pages/HomePage/Home.jsx
@@ -1,4 +1,4 @@
-const HomePage = () => {
+const Home = () => {
   return (
     <div className="home">
         
@@ -6,4 +6,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default Home;
