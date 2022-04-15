2cf9148cb782f38a5f9256fcf48e95fe6443f287
djaafarhamri <hamridjaafar12@gmail.com>
2022-04-15 14:25:48 +0100

    navbar input design

--- views/src/pages/Navbar.jsx
+++ views/src/pages/Navbar.jsx
@@ -1,14 +1,16 @@
-import logo from '../assets/labour.jpeg'
+import logo from "../assets/labour.jpeg";
 
 const Navbar = () => {
-    return ( 
-        <div className="navbar flex h-16 border-solid border center">
-            <img className='h-12' src={logo} alt="" />
-            <input className='h-8' type="text" placeholder='   Search' />
-            <button>Log In</button>
-            <button>Sign Up</button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="navbar flex h-16 border-solid border justify-between items-center mx-32">
+      <img className="h-12 cursor-pointer" src={logo} alt="" />
+      <div>
+        <input className="h-10 w-80 rounded-2xl border-solid border" type="text" placeholder="   Search" />
+        <button className="">Log In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
