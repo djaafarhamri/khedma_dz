9033246edb81826f83830516ee3a8fbbd27e2b95
djaafarhamri <hamridjaafar12@gmail.com>
2022-04-28 18:14:55 +0100

    register step two form starter

--- views/src/pages/RegisterPage/RegisterStepTwo.jsx
+++ views/src/pages/RegisterPage/RegisterStepTwo.jsx
@@ -1,10 +1,20 @@
 const RegisterStepTwo = ({ goToNext }) => {
-    return ( 
-        <div>
-            <h1>step Two</h1>
-            <button onClick={() => goToNext({name: 'djaafar'})}>next</button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="flex flex-col items-center justify-center lg:w-[40%] w-[100%] h-fit m-10 border border-solid">
+      <h1 className="lg:text-3xl text-xl m-10">Sign Up</h1>
+      {/* register form */}
+    
+      <div className="flex w-[80%] justify-between m-3">
+        <input className="w-[45%] py-2 px-4 border border-solid" type="text" placeholder="First Name" />
+        <input className="w-[45%] py-2 px-4 border border-solid" type="text" placeholder="Last Name" />
+      </div>
+      <input className="w-[80%] py-2 px-4 m-3 border border-solid" type="text" placeholder="Email" />
+      <input className="w-[80%] py-2 px-4 m-3 border border-solid" type="text" placeholder="Password" />
+      <button 
+        className="rounded-lg border border-solid border-[#14a800] text-[#fff] bg-[#14a800] m-10 p-2 w-64 font-bold"
+        onClick={() => goToNext({ name: "djaafar" })}>Create My Account</button>
+    </div>
+  );
+};
+
 export default RegisterStepTwo;
