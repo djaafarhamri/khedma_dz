2e21b14b5a48eda10b4d8a3be18b8f8fd45f6489
mekkaoui7 <ahmedmike350@gmail.com>
2022-05-22 00:13:48 +0100

    profile

--- views/src/pages/ProfilePage/Profile.jsx
+++ views/src/pages/ProfilePage/Profile.jsx
@@ -12,11 +12,32 @@ const Profile = () => {
     ) : (
         <div className="profile bg-gray-200 font-inter max-h-screen text-dark-blue mt-20 ">
             <div className="grid sm:grid-cols-12 gap-8 mx-5 py-2">
-
-                <div className=" sm:col-span-12 lg:col-span-3 bg-white p-3 rounded-md">
-                    <img src={img} className="rounded-full h-full w-full object-cover" alt="" />
+                {/*user info */}
+                <div className=" col-span-12 md:col-span-3 bg-white p-3 rounded-md">
+                    <div className="flex flex-col space-y-3 ">
+                      <img src={img} className="rounded-full h-full w-full object-cover" alt="" />
+                      <h1 className="text-center text-2xl font-semibold">Ahmed yacine</h1>
+                      <h2 className="text-xl font-semibold text-left text-gray-500">Age:</h2>
+                      <h2 className="text-xl font-semibold text-left">20</h2>
+                      <h2 className="text-xl font-semibold text-left text-gray-500">Location:</h2>
+                      <h2 className="text-xl font-semibold text-left">saida algeria</h2>
+                      <h2 className="text-xl font-semibold text-left text-gray-500">profession:</h2>
+                      <h2 className="text-xl font-semibold text-left">programer</h2>
+                    </div>
                 </div>
-                <div className="col-start-5 col-end-12">
+                {/*user services and cv */}
+                <div className="col-start-4 col-end-12">
+                    <div className="flex flex-col">
+                        <div className="bg-white text-xl py-4 px-6 font-medium rounded-lg">
+                        A Medical Biller is a medical professional responsible for
+                         managing the administrative responsibilities of billing insurance 
+                         and processing payments for clients. In addition,
+                         they communicate with patients about outstanding balancescollecting
+                          on behalf of their employer or provider institution as necessary to 
+                          ensure the patient’s procedures are paid for by their insurance company
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
