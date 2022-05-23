41483b339133e73e6d654d96e6d065d34df05c83
djaafarhamri <hamridjaafar12@gmail.com>
2022-05-23 10:33:09 +0100

    loader

--- views/src/pages/sharedComponents/Loader.jsx
+++ views/src/pages/sharedComponents/Loader.jsx
@@ -1,8 +1,8 @@
 const Loader = () => {
-    let circleCommonClasses = 'h-2.5 w-2.5 bg-current   rounded-full';
+    let circleCommonClasses = 'h-2.5 w-2.5 bg-green-cyan rounded-full';
 
     return (
-        <div className='flex'>
+        <div className='flex w-screen h-screen justify-center items-center'>
             <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
             <div
                 className={`${circleCommonClasses} mr-1 animate-bounce200`}
